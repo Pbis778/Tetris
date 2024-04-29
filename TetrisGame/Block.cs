495a54ab9e92099adff6264dd734c8f6740b96a1
PioBis778 <pb305347@student.polsl.pl>
2024-04-29 22:20:20 +0200

    gameboard

---
+++ TetrisGame/Block.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TetrisGame
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Sectors { get; }
+        protected abstract Position StartOffSet { get; }
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+
+        public Block()
+        {
+            offset = new Position(StartOffSet.Row, StartOffSet.Column);
+        }
+
+        public IEnumerable<Position> SectorPosition()
+        {
+            foreach (Position pos in Sectors[rotationState])
+            {
+                yield return new Position(pos.Row + offset.Row, pos.Column + offset.Column);
+            }
+        }
+
+        public void RotateBlock()
+        {
+            rotationState = (rotationState + 1) % Sectors.Length;
+        }
+
+        public void RotateBlockCounter()
+        {
+            if (rotationState == 0)
+            {
+                rotationState = Sectors.Length- 1;
+            } else
+            {
+                rotationState--;
+            }
+        }
+
+        public void MoveBlock(int rows, int columns)
+        {
+            offset.Row += rows;
+            offset.Column += columns;
+        }
+
+        public void Reset()
+        {
+            rotationState = 0;
+            offset.Row = StartOffSet.Row;
+            offset.Column = StartOffSet.Column;
+        }
+    }
+}
---
+++ TetrisGame/GameBoard.cs
@@ -0,0 +1,97 @@
+﻿using System.Windows.Controls;
+
+namespace TetrisGame
+{
+    public class GameBoard
+    {
+        private readonly int[,] board;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r, int c]
+        {
+            get => board[r, c];
+            set => board[r, c] = value;
+        }
+
+        public GameBoard(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            board = new int[Rows, Columns];
+        }
+
+        public bool IsInside(int r, int c)
+        {
+            return r >= 0 && r < Rows && c >= 0 && c < Columns;
+        }
+
+        public bool IsEmpty(int r, int c)
+        {
+            return IsInside(r, c) && board[r, c] == 0;
+        }
+
+        public bool IsRowFull(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                if (board[r, c] == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsRowEmpty(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                if (board[r, c] != 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ClearRow(int r)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                board[r, c] = 0;
+            }
+        }
+
+        private void MoveRowDown(int r, int numRows)
+        {
+            for (int c = 0; c < Columns; c++)
+            {
+                board[r + numRows, c] = board[r, c];
+                board[r, c] = 0;
+            }
+        }
+
+        public int ClearFullRow()
+        {
+            int cleared = 0;
+
+            for (int r = Rows-1; r >= 0; r--)
+            {
+                if (IsRowFull(r))
+                {
+                    ClearRow(r);
+                    cleared++;
+                }
+                else if (cleared > 0)
+                {
+                    MoveRowDown(r, cleared);
+                }
+            }
+
+            return cleared;
+        }
+
+
+    }
+}
